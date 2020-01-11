ed60399cee63a45dd49efbd2427573c922f94808
BatchSource <53985751+BatchSource@users.noreply.github.com>
2020-01-11 17:30:26 -0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
 @echo off&cd "%~dp0"
 if "%~1"=="" (title Pytch - Console
-python pytch.py -c
+pytch -c
 ) else (
 title Pytch - "%~1"
-python pytch.py -r "%~1"
+pytch -r "%~1"
 
 echo.&echo.Script has ended.
 pause >nul
